d7173e12e22b094016ef12a6648107c5e4b09604
Naaags <>
2024-04-19 19:58:19 -0400

    Updated backend and api_url

--- api/index.js
+++ api/index.js
@@ -1,14 +1,17 @@
 import express from "express";
-import { createTask, deleteTasks, fetchTasks, updateTask } from "./task.js";
 import serverless from "serverless-http";
 import cors from "cors";
+import { fetchTasks, createTasks, updateTasks, deleteTasks } from "./task.js";
+
 const app = express();
 const port = 3001;
 
-app.use(express.json);
+app.use(express.json());
+
 if (process.env.DEVELOPMENT) {
-  app.use(cors);
+  app.use(cors());
 }
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -16,39 +19,46 @@ app.get("/", (req, res) => {
 app.get("/task", async (req, res) => {
   try {
     const tasks = await fetchTasks();
+
     res.send(tasks.Items);
   } catch (err) {
-    res.status(400).send(`Error fetching tasks:${err}`);
+    res.status(400).send(`Error fetching tasks: ${err}`);
   }
 });
 
 app.post("/task", async (req, res) => {
   try {
-    const tasks = req.body;
-    const response = await createTask(tasks);
+    const task = req.body;
+
+    const response = await createTasks(task);
+
     res.send(response);
   } catch (err) {
-    res.status(400).send(`Error adding tasks:${err}`);
+    res.status(400).send(`Error creating tasks: ${err}`);
   }
 });
 
 app.put("/task", async (req, res) => {
   try {
-    const tasks = req.body;
-    const response = await updateTask(tasks);
+    const task = req.body;
+
+    const response = await updateTasks(task);
+
     res.send(response);
   } catch (err) {
-    res.status(400).send(`Error updating tasks:${err}`);
+    res.status(400).send(`Error updating tasks: ${err}`);
   }
 });
 
 app.delete("/task/:id", async (req, res) => {
   try {
     const { id } = req.params;
+
     const response = await deleteTasks(id);
+
     res.send(response);
   } catch (err) {
-    res.status(400).send(`Error updating tasks:${err}`);
+    res.status(400).send(`Error deleting tasks: ${err}`);
   }
 });
 
--- api/task.js
+++ api/task.js
@@ -2,28 +2,29 @@ import { ListTablesCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import {
   UpdateCommand,
   PutCommand,
-  DynamoDBDocument,
+  DynamoDBDocumentClient,
   ScanCommand,
   DeleteCommand,
-  DynamoDBDocumentClient,
 } from "@aws-sdk/lib-dynamodb";
 import crypto from "crypto";
 
-const client = new DynamoDBClient({ region: "us-east-1" });
+const client = new DynamoDBClient({ region: "us-west-1" });
 const docClient = DynamoDBDocumentClient.from(client);
 
 export const fetchTasks = async () => {
   const command = new ScanCommand({
     ExpressionAttributeNames: { "#name": "name" },
-    ProjectionExpression: "id,#name,complete",
+    ProjectionExpression: "id, #name, completed",
     TableName: "Tasks",
   });
+
   const response = await docClient.send(command);
+
   return response;
 };
 
-export const createTask = async (name, completed) => {
-  const uuid = crypto.randomUUID;
+export const createTasks = async ({ name, completed }) => {
+  const uuid = crypto.randomUUID();
   const command = new PutCommand({
     TableName: "Tasks",
     Item: {
@@ -32,31 +33,43 @@ export const createTask = async (name, completed) => {
       completed,
     },
   });
+
   const response = await docClient.send(command);
+
   return response;
 };
-export const updateTask = async (id, name, completed) => {
+
+export const updateTasks = async ({ id, name, completed }) => {
   const command = new UpdateCommand({
     TableName: "Tasks",
     Key: {
       id,
     },
-    ExpressionAttributeNames: { "#name": "name" },
-    UpdateExpression: "set #name=:n, completed:#c ",
+    ExpressionAttributeNames: {
+      "#name": "name",
+    },
+    UpdateExpression: "set #name = :n, completed = :c",
     ExpressionAttributeValues: {
       ":n": name,
       ":c": completed,
     },
     ReturnValues: "ALL_NEW",
   });
+
   const response = await docClient.send(command);
+
   return response;
 };
-export const deleteTasks = async () => {
-  const command = new DeleteCommand()({
+
+export const deleteTasks = async (id) => {
+  const command = new DeleteCommand({
     TableName: "Tasks",
-    Key: { id },
+    Key: {
+      id,
+    },
   });
+
   const response = await docClient.send(command);
+
   return response;
 };
